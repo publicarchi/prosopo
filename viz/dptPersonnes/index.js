cb41adfa7df280b05605b414f3ce6266eb231951
Lena MK <lena.krause@umontreal.ca>
2021-08-11 17:08:28 -0400

    tous les membres

--- viz/dptPersonnes/index.js
+++ viz/dptPersonnes/index.js
@@ -1,30 +1,60 @@
 const svg = d3.select("#people")
+  .attr("font-family", "sans-serif")
+  .attr("font-size", 10)
 const margin = {top: 40, right: 40, bottom: 40, left: 60};
 const padding = 0;
 const width = +svg.attr("width") - margin.left - margin.right;
-const height = +svg.attr("height") - margin.top - margin.bottom;
+const height = 300;
 
 const fontFamily = "sans-serif";
 const fontScale = 15;
 const rotate = 0 // () => (~~(Math.random() * 6) - 3) * 30
 
-function viewPersonnes(data){
+function showDptPeople(data, y){
+  var localh = y + height;
+  console.log(data.dpt)
+  console.log(localh)
+  var dptView = svg.append("g")
+        .attr("viewBox", [0, localh, width, height])
+        .attr("class", "dpt")
+  
+  dptView 
+        .append("text")
+          .attr("y", localh)
+          .attr("x", margin.left)
+          .text(data.dpt);
 
-  svg.attr("viewBox", [0, 0, width, height])
-      .attr("font-family", fontFamily)
-      .attr("text-anchor", "middle");
+  const g = dptView.append("g")
 
+  const groups = g
+    .selectAll("g")
+    .data(data.roles)
+    .enter()
+    .append("g")
+    .attr("class", "role")
+  
+  groups.each(g => console.log(g))
   
   return svg.node();
 
 }
 
 Promise.all([
-  d3.json('../data/dptPersonnes.json')
-  //version test  d3.json('../data/personnes-test.json')
-]).then(([data]) => {
+  d3.json('../data/dptPersonnes.json'),
+  d3.json('../data/departements-version-simplifiee.geojson')
+]).then(([data, map]) => {
   console.log(data)
+  console.log(map)
   //make a small map as selector
-  viewPersonnes(data);
 
+
+  //for each dpt
+  /*
+  var num = 0;
+  data.forEach(d => {
+    showDptPeople(d, num)
+    num += height;
+  }*/
+
+  showDptPeople(data[0], 0)
 });
--- viz/prosopo/index.js
+++ viz/prosopo/index.js
@@ -10,6 +10,8 @@ const radius = 3;  //rayon des points
 
 //parse le format "xxxx-xx-xx" en date
 const parseDate = d3.timeParse("%Y-%m-%d");
+//intervalle de la chronologie
+var intervalle = ["1733-01-01", "1895-12-31"]
 
 //transforme la date en un string
 function formatDate(date) {
@@ -31,7 +33,30 @@ function createColorScale(values){
   color.domain(Array.from(values));
   console.log(color.domain())
 };
+/*
+function createTooltip(el) {
+  el
+    .style("position", "absolute")
+    .style("pointer-events", "none")
+    .style("top", 0)
+    .style("opacity", 0)
+    .style("background", "white")
+    .style("border-radius", "5px")
+    .style("box-shadow", "0 0 10px rgba(0,0,0,.25)")
+    .style("padding", "10px")
+    .style("line-height", "1.3")
+    .style("font", "11px sans-serif")
+}
 
+function getTooltipContent(d) {
+  return `<b>${d.civilization}</b>
+  <br/>
+  <b style="color:${d.color.darker()}">${d.region}</b>
+  <br/>
+  ${d.name}
+  `
+}
+*/
 
 //crée la base pour la chronologie
 
@@ -70,6 +95,10 @@ function timeline(time, dataset){
     .attr("transform", `translate(0,${height - margin.bottom})`)
     .call(d3.axisBottom(x).ticks(width / 80).tickSizeOuter(0))
 
+  //var x2Axis = (g, x) => g
+  //  .attr("transform", `translate(0,${height - margin.bottom})`)
+  //  .call(d3.axisBottom(x).ticks(width / 80).tickSizeOuter(0))
+
   var yAxis = (g, y) => g
     .attr("transform", `translate(${margin.left},0)`)
     .call(d3.axisLeft(y).ticks(null, "s").tickSize(-(width-margin.left-margin.right)))
@@ -77,6 +106,9 @@ function timeline(time, dataset){
     .call(g => g.selectAll(".tick line").attr("stroke", (d, i) => i ? "#bbb" : null)) 
     //crée une ligne pour chaque y
 
+  
+  //Some interactivity
+  //const tooltip = d3.select(document.createElement("div")).call(createTooltip);
 
   
 
@@ -89,16 +121,47 @@ function timeline(time, dataset){
     .attr("class", "person")
 
   //for each person, addPerson to the timeline
-  people.each(addPerson)
+  people
+    .each(addPerson)
+
+    /*
+    .on("mouseover", function(d) { //some interactivity
+      d3.select(this).select("rect").attr("fill", d.color.darker())
+
+      tooltip
+        .style("opacity", 1)
+        .html(getTooltipContent(d))
+    })
+      .on("mouseleave", function(d) {
+      d3.select(this).select("rect").attr("fill", d.color)
+      tooltip.style("opacity", 0)
+    })*/
 
 
   //Append axes
   const gx = svg.append("g")
-      .call(xAxis, x);
+    .call(xAxis, x);
 
   svg.append("g")
       .call(yAxis, y);
+
   
+  const line = svg.append("line").attr("y1", margin.top-10).attr("y2", height-margin.bottom).attr("stroke", "rgba(0,0,0,0.2)").style("pointer-events","none");
+
+  //some interactivity
+
+  svg.on("mousemove", function(d) {
+
+    let [x,y] = d3.mouse(this);
+    line.attr("transform", `translate(${x} 0)`);
+    y +=20;
+    if(x>width/2) x-= 100;
+
+    /*tooltip
+      .style("left", x + "px")
+      .style("top", y + "px") */
+  }) 
+
   
   return svg.node();
 
@@ -148,10 +211,50 @@ function addPerson(d){
   const death = d.chronometry.filter(t => t.label=="date de décès")[0];
   const points = d.chronometry.filter(t => t.type=="point");
   const ranges = d.chronometry.filter(t => t.type=="range");
-  const startLife = x(parseDate(birth.date));
-  const endLife = x(parseDate(death.date));
+  var startLife;
+  var endLife;
+  //gestion inconnnu dates naissance et mort
+  if (birth && birth.date && death && death.date){
+    //si les deux sont connu
+    startLife = x(parseDate(birth.date)); 
+    endLife = x(parseDate(death.date));
+  }
+  else if (birth && birth.date){
+    //si on connaît la date de naissance mais pas de mort
+    //--> date de naissance + 120ans (Facile à repérer car trop long, à gérer par la suite)
+    //max = debut intervalle
+
+    startLife = x(parseDate(birth.date)); 
+
+    var annee = Number(birth.date.substr(0,4)) + 120;
+    if (annee > Number(intervalle[1].substr(0,4)))
+      endLife = x(parseDate(intervalle[1]));
+    else   
+    endLife = x(parseDate(annee.toString()+"-01-01"));
+
+  }
+  else if (death && death.date){
+    // ex: Bourla date de naissance inconnue à gérer 
+    //--> date de mort - 120ans (Facile à repérer car trop long, à gérer par la suite)
+
+    endLife = x(parseDate(death.date));
+
+    var annee = Number(death.date.substr(0,4)) - 120;
+    if (annee < Number(intervalle[0].substr(0,4)))
+      startLife = x(parseDate(intervalle[0]));
+    else   
+      startLife = x(parseDate(annee.toString()+"-01-01"));
+
+  }
+  else {
+    //si on ne connait rien
+    //les dates de la chronologie, pour signifier l'inconnu facilement
+    startLife = x(parseDate(intervalle[0]));
+    endLife = x(parseDate(intervalle[1]));
+    
+  }
   const widthLife = endLife - startLife;
-  const person = y(d.properties.id)-(y.bandwidth()/2);
+  const person = y(d.properties.id)-(y.bandwidth()+6);
 
 
   //faire un rectangle pour chaque range
@@ -229,8 +332,8 @@ Promise.all([
 
   dataset = data.features.filter(d => vip.has(d.properties.id))
   console.log(dataset)
-  var intervalle = ["1733-01-01", "1895-12-31"]
-  timeline(intervalle, dataset);
+  
+  timeline(intervalle, data.features);
 
 
 });
